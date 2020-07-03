2f9499dc2bc80a479d7c87b6a9c551c9adefd308
Amirabas Ahmadnia <amahmadnia@outlook.com>
2020-07-03 16:40:16 +0430

    UP

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,18 +1,32 @@
         <!-- Blog Sidebar Widgets Column -->
             <div class="col-md-4">
 
-               
-
-                <!-- LOGIN -->
                 <div class="well">
-                    <h4>Login</h4>
-                    <form action="includes/login.php" method="post">
+                    <h4>Blog Search</h4>
+                    <form action="search.php" method="post">
                     <div class="input-group">
                         <input name="search" type="text" class="form-control">
                         <span class="input-group-btn">
                             <button name="submit" class="btn btn-default" type="submit">
                                 <span class="glyphicon glyphicon-search"></span>
                         </button>
+                        </span>
+                    </div>
+                    </form> <!-- serach form-->
+                </div>
+
+                <!-- LOGIN -->
+                <div class="well">
+                    <h4>Login</h4>
+                    <form action="includes/login.php" method="post">
+                    <div class="form-group">
+                        <input name="username" type="text" class="form-control" placeholder="Username...">
+                    </div>
+
+                    <div class="input-group">
+                        <input name="password" type="password" class="form-control" placeholder="Password...">
+                        <span class="input-group-btn">
+                            <button name="submit" class="btn btn-primary btn-default" type="submit">Submit</button>
                         </span>
                     </div>
                     </form> <!-- serach form-->
