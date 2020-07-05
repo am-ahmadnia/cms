1a69bafd4adf71d0125710ac1012096cde18bd29
Amirabas Ahmadnia <amahmadnia@outlook.com>
2020-07-06 00:12:38 +0430

    CMS Dashboard DONE

--- admin/index.php
+++ admin/index.php
@@ -28,7 +28,8 @@
                             
                         </h1>
                     </div>
-                </div>                    
+                </div>       
+                           
                 <!-- /.row -->
                 
         <div class="row">
@@ -40,9 +41,16 @@
                                 <i class="fa fa-file-text fa-5x"></i>
                             </div>
                             <div class="col-xs-9 text-right">
-                        <div class='huge'>12</div>
+                        <div class='huge'>
+                            <?php
+                                $query = "SELECT * FROM posts";
+                                $select_all_posts = mysqli_query($connection,$query);
+                                $post_counts = mysqli_num_rows($select_all_posts);
+                                echo $post_counts;
+                            ?>
+                        </div>
                                 <div>Posts</div>
-                            </div>
+                        </div>
                         </div>
                     </div>
                     <a href="posts.php">
@@ -62,7 +70,14 @@
                                 <i class="fa fa-comments fa-5x"></i>
                             </div>
                             <div class="col-xs-9 text-right">
-                            <div class='huge'>23</div>
+                            <div class='huge'>
+                                <?php
+                                    $query = "SELECT * FROM comments";
+                                    $select_all_comments = mysqli_query($connection,$query);
+                                    $comment_counts = mysqli_num_rows($select_all_comments);
+                                    echo $comment_counts;
+                                ?>
+                            </div>
                             <div>Comments</div>
                             </div>
                         </div>
@@ -84,7 +99,14 @@
                                 <i class="fa fa-user fa-5x"></i>
                             </div>
                             <div class="col-xs-9 text-right">
-                            <div class='huge'>23</div>
+                            <div class='huge'>
+                                <?php
+                                    $query = "SELECT * FROM users";
+                                    $select_all_users = mysqli_query($connection,$query);
+                                    $user_counts = mysqli_num_rows($select_all_users);
+                                    echo $user_counts;
+                                ?>
+                            </div>
                                 <div> Users</div>
                             </div>
                         </div>
@@ -106,23 +128,84 @@
                                 <i class="fa fa-list fa-5x"></i>
                             </div>
                             <div class="col-xs-9 text-right">
-                                <div class='huge'>13</div>
+                                <div class='huge'>
+                                    <?php
+                                        $query = "SELECT * FROM categories";
+                                        $select_all_categories = mysqli_query($connection,$query);
+                                        $categories_counts = mysqli_num_rows($select_all_posts);
+                                        echo $categories_counts;
+                                    ?>
+                                </div>
                                 <div>Categories</div>
                             </div>
                         </div>
                     </div>
                     <a href="categories.php">
-                        <div class="panel-footer">
-                            <span class="pull-left">View Details</span>
-                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="panel-footer">
+                        <span class="pull-left">View Details</span>
+                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                             <div class="clearfix"></div>
                         </div>
                     </a>
                 </div>
             </div>
         </div>
+        
                 <!-- /.row -->
+            <?php
+                $query = "SELECT * FROM posts WHERE post_status = 'draft'";
+                $select_all_draft_posts = mysqli_query($connection,$query);
+                $draft_post_counts = mysqli_num_rows($select_all_draft_posts);   
+                          
+                $query = "SELECT * FROM comments WHERE comment_status = 'unapproved'";
+                $select_all_unapproved_comments = mysqli_query($connection,$query);
+                $unapproved_comment_counts = mysqli_num_rows($select_all_unapproved_comments);   
+                
+                $query = "SELECT * FROM users WHERE user_role = 'subscriber'";
+                $select_all_subscribers = mysqli_query($connection,$query);
+                $subscriber_counts = mysqli_num_rows($select_all_subscribers);
+                
+                // $query = "SELECT * FROM users WHERE user_role = 'subscriber'";
+                // $select_all_subscribers = mysqli_query($connection,$query);
+                // $subscriber_counts = mysqli_num_rows($select_all_subscribers);
 
+            ?>
+            <div class="row">
+                <script type="text/javascript">
+                    google.charts.load('current', {'packages':['bar']});
+                    google.charts.setOnLoadCallback(drawChart);
+
+                    function drawChart() {
+                        var data = google.visualization.arrayToDataTable([
+                        ['Date', 'Count'],
+                        <?php
+                            $element_text = ['Active Posts', 'Draft Posts', 'Categories', 'Users', 'Subscribers', 'Comments', 'Pending Comments'];
+                            $element_count = [$post_counts, $draft_post_counts, $categories_counts, $user_counts, $subscriber_counts, $comment_counts, $unapproved_comment_counts];
+                            
+                            for ($i=0; $i < 7; $i++) { 
+                                echo "['$element_text[$i]'" . "," . "$element_count[$i]],";
+                            }
+                            // var_dump($element_text);
+                        ?>
+                        
+
+                        ]);
+
+                        var options = {
+                        chart: {
+                            title: '',
+                            subtitle: '',
+                        }
+                        };
+
+                        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                        chart.draw(data, google.charts.Bar.convertOptions(options));
+                    }
+                </script>
+                <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
+
+            </div>
 
             </div>
             <!-- /.container-fluid -->
